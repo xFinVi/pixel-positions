a8711f26a95bbba294e342a03aea5aa704c415d0
Filippos <philterzidis@hotmail.com>
2024-08-21 14:13:06 +0100

    Search input

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,9 +7,13 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
-        $jobs = Job::where('title', 'LIKE', '%' . request('q') . '%')->get();
+        // Retrieve the search query
+        $query = $request->input('q');
+
+        // Perform case-insensitive search using LOWER() function
+        $jobs = Job::whereRaw('LOWER(title) LIKE ?', ['%' . strtolower($query) . '%'])->get();
 
         return view('results', ['jobs' => $jobs]);
     }
